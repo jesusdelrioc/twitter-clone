2c80aced2a18f29f73a5182aa4baff636e653c5c
jesusdelrioc <jesusdelrioc@gmail.com>
2018-02-06 10:36:12 +0100

    iteraccion 1

---
+++ twitter-clone/bin/seed-user.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
+const User = require("../models/User");
+
+mongoose.connect('mongodb://localhost/twitter-clone');
+
+const salt= bcrypt.genSaltSync(bcryptSalt);
+const password ="1234";
+const encrypted= bcrypt.hashSync(password, salt);
+
+const users = [
+    {
+        username:"jesus",
+        password: encrypted
+    },{
+        username:"ana",
+        password: encrypted
+    },{
+        username:"alvaro",
+        password: encrypted
+    }
+]
+User.create(users, (err, docs) => {
+    if(err){
+        throw err
+    }
+    docs.forEach((user)=>{
+        console.log(user.username)
+    } )
+    mongoose.connection.close()
+});
